2c48e045035b586645f2c208fd11c0028c8a9e51
Fabian <19959@ma-web.nl>
2020-03-13 17:21:59 +0100

    add files

---
+++ js/fotobox.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $('.fotobox').slick({
+        fade: true,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        cssEase: 'linear',
+    });
+});
